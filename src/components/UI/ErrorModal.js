f6bd5c45d400539c7d3e29bfe05977ce43d2996d
Pranjal Gupta <pranjalg131@gmail.com>
2021-06-21 02:33:49 +0000

    Added Error Modal to handle input errors

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+
+const ErrorModal = ({ title, message, onConfirm }) => (
+  <div>
+    <div
+      className={classes.backdrop}
+      onClick={onConfirm}
+      onKeyDown={onConfirm}
+      aria-hidden="true"
+    />
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onConfirm}> Okay </Button>
+      </footer>
+    </Card>
+  </div>
+);
+
+export default ErrorModal;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 
 import classes from './AddUser.module.css';
 
 const AddUser = ({ onSuccess }) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [errorState, setErrorState] = useState();
 
   const isValidInput = (username, age) => {
     // Handling Empty Inputs
-    if (username.trim().length === 0 || age.trim().length === 0) return false;
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      setErrorState({
+        title: 'Invalid Inputs',
+        message: 'Please enter valid username and age',
+      });
+      return false;
+    }
     // Handling correct Ages
-    if (+age <= 0) return false;
+    if (+age <= 0) {
+      setErrorState({
+        title: 'Invalid Age',
+        message: 'Please enter a valid age (> 0)',
+      });
+      return false;
+    }
 
     return true;
   };
@@ -34,28 +48,41 @@ const AddUser = ({ onSuccess }) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setErrorState(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        {/* The value attribute is used for 'double-binding' */}
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
+    <div>
+      {errorState && (
+        <ErrorModal
+          title={errorState.title}
+          message={errorState.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor="age">Age (In Years)</label>
-        {/* The value attribute is used for 'double-binding' */}
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          {/* The value attribute is used for 'double-binding' */}
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          />
+          <label htmlFor="age">Age (In Years)</label>
+          {/* The value attribute is used for 'double-binding' */}
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
