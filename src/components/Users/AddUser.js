82b3ea191b041bbe7a1962bebfb16d12f2595dc6
Pranjal Gupta <pranjalg131@gmail.com>
2021-06-22 02:59:35 +0000

    Added Ref's to the addUser Form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
@@ -6,12 +6,16 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = ({ onSuccess }) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
   const [errorState, setErrorState] = useState();
+
+  // Using Ref's to get user input.
+
+  const usernameInput = useRef();
+  const ageInput = useRef();
 
   const isValidInput = (username, age) => {
     // Handling Empty Inputs
+
     if (username.trim().length === 0 || age.trim().length === 0) {
       setErrorState({
         title: 'Invalid Inputs',
@@ -19,7 +23,9 @@ const AddUser = ({ onSuccess }) => {
       });
       return false;
     }
+
     // Handling correct Ages
+
     if (+age <= 0) {
       setErrorState({
         title: 'Invalid Age',
@@ -33,19 +39,19 @@ const AddUser = ({ onSuccess }) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (isValidInput(enteredUsername, enteredAge)) {
-      onSuccess(enteredUsername, enteredAge);
-      setEnteredUsername('');
-      setEnteredAge('');
+
+    // On submission only, the input is read and not on every keystroke.
+
+    const username = usernameInput.current.value;
+    const age = ageInput.current.value;
+    if (isValidInput(username, age)) {
+      onSuccess(username, age);
+
+      // Technically ref's should not be used to modify the DOM element , but in this case its okay as we are only clearing the fields.
+
+      usernameInput.current.value = '';
+      ageInput.current.value = '';
     }
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
   };
 
   const errorHandler = () => {
@@ -65,20 +71,10 @@ const AddUser = ({ onSuccess }) => {
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
           {/* The value attribute is used for 'double-binding' */}
-          <input
-            id="username"
-            type="text"
-            onChange={usernameChangeHandler}
-            value={enteredUsername}
-          />
+          <input id="username" type="text" ref={usernameInput} />
           <label htmlFor="age">Age (In Years)</label>
           {/* The value attribute is used for 'double-binding' */}
-          <input
-            id="age"
-            type="number"
-            onChange={ageChangeHandler}
-            value={enteredAge}
-          />
+          <input id="age" type="number" ref={ageInput} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
